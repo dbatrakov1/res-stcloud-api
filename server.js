bc9b358c90dcb69cca98d77abe5047664f3c52d2
denis laptop <dbatrakov1@gmail.com>
2022-06-03 19:24:14 -0500

    random

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 const PORT = 8000
+
+
 
 const rappers = {
     '21 savage': {
@@ -19,9 +22,24 @@ const rappers = {
         'birthLocation': 'unknown'
     }
 }
+let randNumberForObject = Math.floor(Math.random() * (Object.keys(rappers).length))
+
+//function return random key (Each key-value pair is called a property.)
+function getRandomProperty(obj) {
+    const keys = Object.keys(obj);
+    return keys[Math.floor(Math.random() * keys.length)];
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 app.get('/', (request, response)=>{
     response.sendFile(__dirname + '/index.html')
 })
+
+//sent rendom property
+app.get('/random', (request, response) =>{
+    response.json(rappers[getRandomProperty(rappers)])
+})
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 app.get('/api/:name',(request,response)=>{
     const rapperName = request.params.name.toLowerCase()
@@ -36,4 +54,6 @@ app.get('/api/:name',(request,response)=>{
 
 app.listen(PORT, ()=>{
     console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
+    console.log(Object.keys(rappers).length)//return object rappers length
+    console.log(randNumberForObject)
 })
