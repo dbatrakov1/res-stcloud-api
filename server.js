43afb272b20c108b2159c767dbd62eac4148acf4
Denis <dbatrakov1@gmail.com>
2022-06-03 21:06:30 -0500

    try use what heroku set for a port, if it does not use const PORT

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/api/:name',(request,response)=>{
     
 })
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
     console.log(Object.keys(rappers).length)//return object rappers length
     console.log(randNumberForObject)
